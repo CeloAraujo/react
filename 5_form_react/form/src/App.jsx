acee237ebee525204f535dc4f56b9f4a6d88d9e3
celoaraujo <celoparaujo99@gmail.com>
2024-04-23 18:14:51 -0300

    Form em react

---
+++ 5_form_react/form/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import "./App.css";
+import MyForm from "./components/MyForm";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Form em React</h1>
+      </div>
+      <MyForm />
+    </>
+  );
+}
+
+export default App;
