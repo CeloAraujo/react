9c1168bf8d402eb29fbd675ef3880ee0e5414d59
celoaraujo <celoparaujo99@gmail.com>
2024-05-13 17:14:49 -0300

    input e useState

--- 9_hooks/hook/src/components/HookUseState.jsx
+++ 9_hooks/hook/src/components/HookUseState.jsx
@@ -7,7 +7,15 @@ const HookUseState = () => {
 
   const changeNames = () => {
     setName("Marcelo Araujo");
-  }
+  };
+  //   2- useState e input
+  const [age, setAge] = useState(18);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(age);
+  };
   return (
     <div>
       {/* 1-useState */}
@@ -15,6 +23,17 @@ const HookUseState = () => {
       <p>Variável: {userName}</p>
       <p>useState: {name}</p>
       <button onClick={changeNames}>Mude nome</button>
+      {/* 2- useState e input */}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={age}
+          onChange={(e) => setAge(e.target.value)}
+        />
+        <input type="submit" value="Enviar" />
+        <p>Você tem {age} anos</p>
+        <hr />
+      </form>
     </div>
   );
 };
