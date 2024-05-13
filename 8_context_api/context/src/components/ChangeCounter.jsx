79c1a1c99328fb47791f517e93f1f267c4299ed5
celoaraujo <celoparaujo99@gmail.com>
2024-05-13 16:07:00 -0300

    refatorando contexto com hook

--- 8_context_api/context/src/components/ChangeCounter.jsx
+++ 8_context_api/context/src/components/ChangeCounter.jsx
@@ -1,8 +1,11 @@
 import { useContext } from "react";
-import { CounterContext } from "../context/CounterContext";
+// import { CounterContext } from "../context/CounterContext";
+// 4- refatorando com hook
+import { useCounterContext } from "../hooks/useCounterContext";
 
 const ChangeCounter = () => {
-  const { counter, setCounter } = useContext(CounterContext);
+  //   const { counter, setCounter } = useContext(CounterContext);
+  const { counter, setCounter } = useCounterContext();
 
   return (
     <div>
--- 8_context_api/context/src/routes/Contact.jsx
+++ 8_context_api/context/src/routes/Contact.jsx
@@ -1,8 +1,12 @@
 import { useContext } from "react";
 import { CounterContext } from "../context/CounterContext";
 
+// 4- refatorando com hook
+import { useCounterContext } from "../hooks/useCounterContext";
+
 const Contact = () => {
-  const { counter } = useContext(CounterContext);
+  // const { counter } = useContext(CounterContext);
+  const { counter } = useCounterContext();
   return (
     <div>
       <h1>Página de controle</h1>
