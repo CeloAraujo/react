f23417be70517a71447d30253b9c47049579fd2d
celoaraujo <celoparaujo99@gmail.com>
2024-05-15 16:14:32 -0300

    useRef

--- 9_hooks/hook/src/routes/Home.jsx
+++ 9_hooks/hook/src/routes/Home.jsx
@@ -5,6 +5,7 @@ import HookUseEffect from "../components/HookUseEffect";
 
 import { useContext } from "react";
 import { SomeContext } from "../components/HookUseContext.jsx";
+import HookUseRef from "../components/HookUseRef.jsx";
 
 const Home = () => {
   const { contextValue } = useContext(SomeContext);
@@ -17,6 +18,7 @@ const Home = () => {
       <h2>useContext</h2>
       <p>Valor do contexto: {contextValue}</p>
       <hr />
+      <HookUseRef/>
     </div>
   );
 };
